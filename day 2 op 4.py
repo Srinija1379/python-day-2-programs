b752d2e24d0389e518573f9866efb271b4528c13
Srinija1379 <112627711+Srinija1379@users.noreply.github.com>
2022-10-07 13:33:33 +0530

    Add files via upload

---
+++ day 2 op 4.py
@@ -0,0 +1,23 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\merge sort program.py 
+the list 1 is: [0, 1, 9]
+the combined sortedlist is: [0, 1, 3, 4, 5, 9]
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\merge sort program.py 
+the list 1 is: [1, 2, 4]
+the combined sortedlist is: [1, 1, 2, 3, 4, 4]
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\merge sort program.py 
+the list 1 is: []
+the combined sortedlist is: []
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\merge sort program.py 
+the list 1 is: []
+the combined sortedlist is: [0]
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\merge sort program.py 
+the list 1 is: [9, 8, 7]
+the combined sortedlist is: [0, 1, 3, 7, 7, 8, 9]
+>>> 
