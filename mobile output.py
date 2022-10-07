8e45b314669b15f9b57d8e27360f5ba22641a881
Srinija1379 <112627711+Srinija1379@users.noreply.github.com>
2022-10-07 14:21:44 +0530

    Add files via upload

---
+++ mobile output.py
@@ -0,0 +1,24 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/mobile digits.py 
+Enter the digit: 23
+[" de ",]
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/mobile digits.py 
+Enter the digit: ""
+]
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\maximum word count.py 
+Enter the total sentences: 2
+Enter the sentences: 
+87
+
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/mobile digits.py 
+Enter the digit: 2
+[" 2 "," 3 "," 4 "," 5 "," 6 "," 7 "," 8 "," 9 ",]
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/mobile digits.py 
+Enter the digit: 87
+[" pq ",]
+>>> 
