4d0be5f2ca45ba0923fc0e8aafbb01c9136a0924
Srinija1379 <112627711+Srinija1379@users.noreply.github.com>
2022-10-07 14:35:03 +0530

    Add files via upload

---
+++ common output.py
@@ -0,0 +1,21 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\common in two strings.py 
+s1=sky is blue in colour
+s2=sri likes sky blue colour
+is in
+sri likes
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\common in two strings.py 
+s1=learn python
+s2=python is east to leran
+learn
+is east to leran
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\common in two strings.py 
+s1=sri likes apple
+s2=apple is red in colour
+sri likes
+is red in colour
+>>> 
